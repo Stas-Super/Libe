ddfb187722c12d61759a96355a8aeda0b82ccca1
Stanislav <skorchevskyi@zeushq.com>
2021-05-19 10:17:27 +0300

    Create Identity

---
+++ Libe.DAL/Entities/Role.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Libe.DAL.Entities
+{
+    public class Role : IdentityRole
+    {
+    }
+}
